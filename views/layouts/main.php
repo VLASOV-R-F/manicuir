2c1aafc0f3d46088fd61eb7dc66a29a6e8b9d889
VLASOV-R-F <rimark.ek@mail.ru>
2019-01-12 21:41:08 +0500

    Убрал подключение файлов стилей и скриптов, подключил страницу к движку

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -1,32 +1,36 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: rimark-nout
- * Date: 07.01.2019
- * Time: 19:15
- */
+
+/* @var $this \yii\web\View */
+/* @var $content string */
+
+use app\widgets\Alert;
+use yii\helpers\Html;
+use yii\bootstrap\Nav;
+use yii\bootstrap\NavBar;
+use yii\widgets\Breadcrumbs;
+use app\assets\AppAsset;
+
+AppAsset::register($this);
 ?>
 
-<!-- Page Title--><!DOCTYPE html>
-<html class="wide wow-animation" lang="en">
+<?php $this->beginPage() ?>
+<html lang="<?= Yii::$app->language ?>">
   <head>
-    <title>Nail Salon </title>
-    <meta name="format-detection" content="telephone=no">
-    <meta name="viewport" content="width=device-width, height=device-height, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
+    <meta charset="<?= Yii::$app->charset ?>">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <?= Html::csrfMetaTags() ?>
+    <title><?= Html::encode($this->title) ?></title>
+    <?php $this->head() ?>
     <link rel="icon" href="images/favicon.ico" type="image/x-icon">
     <!-- Stylesheets-->
-    <link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Lato:300i,400,400i,700%7CMontserrat:400,500,600,700%7CPlayfair+Display:400,700,700i%7COswald:400,700,700i">
-    <link rel="stylesheet" href="css/bootstrap.css">
-    <link rel="stylesheet" href="css/fonts.css">
-    <link rel="stylesheet" href="css/style.css">
     <!--[if lt IE 10]>
     <div style="background: #212121; padding: 10px 0; box-shadow: 3px 3px 5px 0 rgba(0,0,0,.3); clear: both; text-align:center; position: relative; z-index:1;"><a href="http://windows.microsoft.com/en-US/internet-explorer/"><img src="images/ie8-panel/warning_bar_0000_us.jpg" border="0" height="42" width="820" alt="You are using an outdated browser. For a faster, safer browsing experience, upgrade for free today."></a></div>
     <script src="js/html5shiv.min.js"></script>
     <![endif]-->
   </head>
   <body>
+  <?php $this->beginBody() ?>
     <div class="preloader">
       <div class="preloader-body">
         <div class="cssload-container">
@@ -624,9 +628,7 @@
     </div>
     <!-- Global Mailform Output-->
     <div class="snackbars" id="form-output-global"></div>
-    <!-- Javascript-->
-    <script src="js/core.min.js"></script>
-    <script src="js/script.js"></script>
-    <!-- coded by barber-->
+  <?php $this->endBody() ?>
   </body>
 </html>
+<?php $this->endPage() ?>
